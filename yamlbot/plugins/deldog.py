d878a36b16d596750a167ba2ba41217460191517
pokurt <aman@virtual.mv>
2020-09-26 04:59:12 +0500

    fixed errors

--- yamlbot/plugins/deldog.py
+++ yamlbot/plugins/deldog.py
@@ -5,11 +5,11 @@ async def deldog(message, data):
     BASE_URL = 'https://del.dog'
     r = requests.post(f'{BASE_URL}/documents', data=data.encode('utf-8'))
     if r.status_code == 404:
-        await message.edit('Failed to reach dogbin')
+        print('Failed to reach dogbin')
         r.raise_for_status()
     res = r.json()
     if r.status_code != 200:
-        await message.edit(res['message'])
+        print(res['message'])
         r.raise_for_status()
     key = res['key']
     if res['isUrl']:
