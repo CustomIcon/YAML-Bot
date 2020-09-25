33b765c8738049a8dc1f5b2b5288c7ee73702e32
pokurt <aman@virtual.mv>
2020-09-26 02:02:25 +0500

    cleanup

--- yamlbot/plugins/yamler.py
+++ yamlbot/plugins/yamler.py
@@ -17,31 +17,12 @@ async def aexec(code, client, message):
     return await locals()["__aexec"](client, message)
 
 
-@YamlBot.on_message(~filters.private & filters.command('yaml'))
+@YamlBot.on_message(~filters.private & filters.command(['yaml', 'yaml@pyrogramyamlbot']))
 @YamlBot.on_message(filters.private)
 async def prettyprint(client, message):
     cmd = f'p(message, stream=sys.stdout)'
-    old_stderr = sys.stderr
-    old_stdout = sys.stdout
     redirected_output = sys.stdout = StringIO()
-    redirected_error = sys.stderr = StringIO()
-    stdout, stderr, exc = None, None, None
-    try:
-        await aexec(cmd, client, message)
-    except Exception:
-        exc = traceback.format_exc()
-    stdout = redirected_output.getvalue()
-    stderr = redirected_error.getvalue()
-    sys.stdout = old_stdout
-    sys.stderr = old_stderr
-    evaluation = ""
-    if exc:
-        evaluation = exc
-    elif stderr:
-        evaluation = stderr
-    elif stdout:
-        evaluation = stdout
-    else:
-        evaluation = "Success"
+    await aexec(cmd, client, message)
+    evaluation = redirected_output.getvalue()
     final_output = f"```{evaluation.strip()}```"
     await message.reply_text(final_output)
