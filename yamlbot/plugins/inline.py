a1a0c7e43ac34101138c5430762d81306dcecc94
ポキ <aman_a@aol.com>
2021-05-24 23:25:45 +0500

    type hints

--- yamlbot/plugins/inline.py
+++ yamlbot/plugins/inline.py
@@ -2,12 +2,12 @@ from yamlbot import YamlBot
 from yamlbot.plugins.yamler import aexec
 from io import StringIO
 import sys
+from pyrogram import types
 from pyrogram.errors.exceptions.bad_request_400 import UserIsBlocked
 
 
 @YamlBot.on_inline_query()
-async def inline_query_handler(client, message):
-    answers = []
+async def inline_query_handler(client: YamlBot, message: types.InlineQuery):
     cmd = 'p(message, stream=sys.stdout)'
     redirected_output = sys.stdout = StringIO()
     await aexec(cmd, client, message)
@@ -16,14 +16,16 @@ async def inline_query_handler(client, message):
     try:
         await client.send_message(message.from_user.id, final_output)
         await client.answer_inline_query(message.id,
-                                        results=answers,
+                                        results=[],
                                         switch_pm_text="Output was sent to pm",
-                                        switch_pm_parameter="start"
+                                        switch_pm_parameter="start",
+                                        cache_time=0
                                         )
     except UserIsBlocked:
         await client.answer_inline_query(message.id,
-                                        results=answers,
+                                        results=[],
                                         switch_pm_text="Unblock me to get InlineQuery Yaml",
-                                        switch_pm_parameter="start"
+                                        switch_pm_parameter="start",
+                                        cache_time=0
                                         )
     
